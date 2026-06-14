afb0d6c7995678b2e69a71880c61708fbd953bb3
Vishal <vishaltrycode@gmail.com>
2026-06-14 19:29:07 +0530

    print the value of a and b

--- Test/src/com/java/Example.java
+++ Test/src/com/java/Example.java
@@ -6,6 +6,8 @@ public class Example {
 		int a = 10;
 		int b = 20;
 		int c = a + b;
+		System.out.println("value of a :" + a);
+		System.out.println("value of b :" + b);
 		System.out.println("Addition is :" + c);
 	}
 }
